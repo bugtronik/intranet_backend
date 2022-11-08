61380949ebb4a7c5cf8050dee9c305bbcc01b9fd
Aly <aly.mboumba@setrag.com>
2022-11-08 14:20:52 +0100

    Init project

---
+++ intranet_backend/src/main/java/com/setrag/intranet_backend/IntranetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.setrag.intranet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetBackendApplication.class, args);
+	}
+
+}
---
+++ intranet_backend/src/test/java/com/setrag/intranet_backend/IntranetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setrag.intranet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
